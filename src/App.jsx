a8e0de5aaa0876837dd917f1a09f82a752b71510
KJRai <alexrayee289@gmail.com>
2025-01-30 10:24:47 +0545

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,68 @@
 import "./App.css";
-import { ShoppingListItem } from "./components/ShoppingListItem";
+import { useState } from "react"
+import  ShoppingListItem  from "./components/ShoppingListItem"
 
-function App() {
+export default function App() {
+
+  const [items, setItems] = useState([])
+
+  const [newItem, setNewItem] = useState("")
+
+  const addItems = (e) => {
+    e.preventDefault();
+    const isDuplicate = items.some(
+      (item) => item.name.toLowerCase() === newItem.toLowerCase()
+    );
+    
+    if (isDuplicate) {
+      alert("This item already exists in the list.");
+      setNewItem("");
+      return;
+    }
+      setItems(prevItems => [...prevItems, { name: newItem, completed: false }]);
+      setNewItem("");
+  };
+
+  const toggleCompleted = (index) => {
+    setItems((prevItems) =>
+      prevItems.map((item, i) =>
+        i === index ? { ...item, completed: !item.completed } : item
+      )
+    );
+  };
+  const removeItems = (index) => {
+    setItems((prevItems) => prevItems.filter((_, i) => i !== index));
+  };
   return (
     <div className="container">
       <h1 className="mb-4">My Shopping List</h1>
 
       <div className="flex gap-4 pb-3 border-b-2 border-gray-700">
-        <input
-          type="text"
-          placeholder="E.g. Carrots"
-          className="v__input flex-1"
-        />
-        <button className="v__button">Add</button>
+        <form onSubmit={addItems} className="add-item-form">
+          <input
+            type="text"
+            placeholder="E.g. Carrots"
+            className="form-input"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            name="item"
+          />
+          <button className="v__button" type="submit" disabled = {newItem === ""} >Add</button>
+        </form>
       </div>
       <div className="v__list-container overflow-y-scroll">
-        {/* Map your data here: */}
-        <ShoppingListItem />
-        <ShoppingListItem />
+        <li>
+          {items.map((item, index) => (
+            <ShoppingListItem
+              key={index}
+              item={item}
+              ontoggleCompleted={() => toggleCompleted(index)}
+              onRemove={() => removeItems(index)}
+            />
+          ))}
+        </li>
       </div>
     </div>
   );
 }
 
-export default App;
--- src/components/ShoppingListItem.jsx
+++ src/components/ShoppingListItem.jsx
@@ -1,11 +1,38 @@
 import classes from "./ShoppingListItem.module.css";
+import React from "react";
 
-export const ShoppingListItem = () => {
+export default function ShoppingListItem(props){
+  const { item, ontoggleCompleted, onRemove } = props
+  console.log(ontoggleCompleted)
   return (
-    <div className="flex items-center p-2">
-      <input type="checkbox" className="mr-2" />
-      <h3 className="flex-1">Carrots</h3>
-      <button className={classes.removeButton}>x</button>
+    
+    <div style={{ 
+      display: "flex", 
+      alignItems: "center",
+      justifyContent: "space-between",
+      padding: "10px"
+      }}>
+      <input 
+        type="checkbox" 
+        className="mr-2"
+        checked = {item.completed || false}
+        onChange={ontoggleCompleted}
+        
+         />
+      <h3 className={`flex-1 ${
+        item.completed ? 
+        classes.completed : ""
+      }`}
+      
+      style={{
+        textDecoration: item.completed ? "line-through" : "none",
+        fontSize: "1.5rem",
+       }}>
+
+    
+      {item.name}
+      </h3>
+      <button className={classes.removeButton} onClick={onRemove}>x</button>
     </div>
   );
-};
+}

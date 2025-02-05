3102cd2bfb476c426d7482bba62ade7622fd54d5
KJRai <alexrayee289@gmail.com>
2025-02-05 09:39:26 +0545

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,99 @@
 import "./App.css";
 import { Album, Photo } from "./components";
-
-const DUMMY_DATA = [
-  {
-    albumId: 1,
-    id: 1,
-    title: "accusamus beatae ad facilis cum similique qui sunt",
-    url: "https://via.placeholder.com/600/92c952",
-    thumbnailUrl: "https://via.placeholder.com/150/92c952",
-  },
-  {
-    albumId: 1,
-    id: 2,
-    title: "reprehenderit est deserunt velit ipsam",
-    url: "https://via.placeholder.com/600/771796",
-    thumbnailUrl: "https://via.placeholder.com/150/771796",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import MoonLoader from "react-spinners/MoonLoader";
 
 function App() {
-  // you can make use of the following to get the base url
-  console.log(import.meta.env.VITE_BASE_URL);
+  const [selectedAlbumId, setSelectedAlbumId] = useState(null);
+
+  const {
+    data: albums,
+    isLoading: isAlbumsLoading,
+    isError: isAlbumsError,
+  } = useQuery({
+    queryKey: ["albums"],
+    queryFn: async () => {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/albums`);
+      return response.data;
+    },
+  });
+
+  const {
+    data: photos,
+    isLoading: isPhotosLoading,
+    isError: isPhotosError,
+  } = useQuery({
+    queryKey: ["photos", selectedAlbumId],
+    queryFn: async () => {
+      if (!selectedAlbumId) return [];
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/albums/${selectedAlbumId}/photos`
+      );
+      return response.data;
+    },
+    enabled: !!selectedAlbumId,
+  });
+
+  const handleAlbumClick = (albumId) => {
+    setSelectedAlbumId((prevId) => (prevId === albumId ? null : albumId));
+  };
 
   return (
     <div className="container">
       <div className="flex gap-4">
+        
         <div>
-          <h1 className="pb-4 border-b-2 mb-4 sticky">Albums</h1>
-
+          <h1 className="bg-cyan-500 pb-4 border-b-2 mb-4 sticky ">Albums</h1>
           <div className="flex flex-col gap-2 h-screen overflow-y-auto pr-2">
-            {/* Get Album data from https://jsonplaceholder.typicode.com/albums */}
-            <Album />
-            <Album />
-          </div>
-        </div>
-        <div className="flex-1">
-          <h1 className="pb-4 border-b-2 mb-4">Photos</h1>
-
-          <div className="grid grid-cols-4 gap-4">
-            {/* Only show this initially */}
-            <p className="col-span-4 text-gray-500">
-              Click on an album to start viewing photos.
-            </p>
-
-            {DUMMY_DATA.map((photo, index) => (
-              <Photo {...photo} key={index} />
-            ))}
-
-            {!DUMMY_DATA.length && (
-              <p className="col-span-4">No photos found in this album</p>
+            {isAlbumsLoading ? (
+              <p>Loading albums...</p>
+            ) : isAlbumsError ? (
+              <p>Error loading albums.</p>
+            ) : (
+              albums.map((album) => (
+                <div key={album.id}>
+                  <Album
+                    title={album.title}
+                    onClick={() => handleAlbumClick(album.id)}
+                  />
+                  {selectedAlbumId === album.id && (
+                    <div className="photos-grid">
+                      <h1 className="pb-4 border-b-2 mb-4">Photos</h1>
+                      <div className="grid-container">
+                        {isPhotosLoading ? (
+                          <p className="flex justify-center items-center"><MoonLoader 
+                              size={60} 
+                              color="#3b82f6" 
+                              loading={isPhotosLoading}/></p>
+                        ) : isPhotosError ? (
+                          <p>Error loading photos.</p>
+                        ) : photos && photos.length > 0 ? (
+                          photos.map((photo) => (
+                            <Photo
+                              key={photo.id}
+                              url={photo.thumbnailUrl} 
+                              title={photo.title}
+                            />
+                          ))
+                        ) : (
+                          <p className="col-span-4 text-gray-500">
+                            {selectedAlbumId
+                              ? "No photos found in this album."
+                              : "Click on an album to start viewing photos."}
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))
             )}
           </div>
         </div>
       </div>
-      {/* <div className="flex">
-        <img src="/vite.svg" className="v__logo" alt="Vite logo" />
-        <img src={reactLogo} className="v__logo react" alt="React logo" />
-      </div> */}
     </div>
   );
 }
--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -2,8 +2,10 @@ import classes from "./Photo.module.css";
 
 export const Photo = ({ title, url }) => {
   return (
-    <div className={classes.container}>
-      <img src={url} className={classes.image} alt="Vite logo" />
+    <div className={classes.container} style={{
+      backgroundColor: "#316f7e",
+    }}>
+      <img src={url} className={classes.image} alt={title} />
       <p className={classes.caption}>{title}</p>
     </div>
   );
